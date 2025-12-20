4a6829dbd878d7ce1aac13b1f613c4c145c2820d
seejay <seejay181@gmail.com>
2025-12-19 21:24:46 -0600

    removing tests as they haven't been run in a while

--- server/testOllama/testConOllama.sh
+++
@@ -1,3 +0,0 @@
-seq 1 5 | parallel -j2 'curl -s http://localhost:11434/api/generate \
-  -H "Content-Type: application/json" \
-  -d "{\"model\": \"tinyllama\", \"prompt\": \"Hello Pi!\", \"stream\": false}"'
